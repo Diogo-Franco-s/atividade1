879ce0366533aa8ffb2a680a004fd5f4c380e748
“Arthur <arthurrampi@gmail.com>
2023-07-31 20:22:34 -0300

    cadastro classe ProdutoDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -14,11 +14,19 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    conn = new conectaDAO().connectDB();
+    String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)";
+     
+    try {
+    PreparedStatement stmt = this.conn.prepareStatement(sql);
+    stmt.setString(1, produto.getNome());
+    stmt.setDouble(2, produto.getValor());                      
+    stmt.setString(3, produto.getStatus());
+    stmt.execute();
+    System.out.println("cadastro realizado com sucesso.");
+      } catch (Exception e) {
+      System.out.println("Erro ao cadastrar: " + e.getMessage());
+      }    
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
